5fef40a23efe5cb5df6389dfecbc018c3ebc4b9b
Facundo1105 <FACUNDO.ROSALES@CORREO.UCU.EDU.UY>
2025-05-30 02:42:40 -0300

    Hice las clases de estructuras y su intefaz

---
+++ src/Library/Estructuras/EstructurasUnidades/Establo.cs
@@ -0,0 +1,40 @@
+namespace Library;
+
+public class Establo : IEstructuras
+{
+    private int vida = 1500;
+
+    public string Nombre
+    {
+        get
+        {
+            return "Establo";
+        }
+    }
+
+    public int Vida
+    {
+        get
+        {
+            return this.vida;
+        }
+        set
+        {
+            this.vida = value < 0 ? 0 : value;
+        }
+    }
+
+    public void CrearCaballeria(Jugador jugador)
+    {
+        if (jugador.Poblacion < 50 && jugador.CantidadUnidades < 30)
+        {
+            if (jugador.Recursos["Oro"] >= 200 && jugador.Recursos["Alimento"] >= 300 && jugador.Recursos["Madera"] >= 100)
+            {
+                jugador.Recursos["Oro"] -= 200;
+                jugador.Recursos["Alimento"] -= 300;
+                jugador.Recursos["Madera"] -= 100;
+                jugador.Caballeria.Add(new Caballeria());
+            }
+        }
+    }
+}
