18b532880dec49c601a17703a5981cbd7d6ffb99
ignacioviera34 <IGNACIO.VIERA@correo.ucu.edu.uy>
2025-05-31 18:47:41 -0300

    agregue las clases de civilizaciones

---
+++ src/Library/Civilizaciones/ICivilizacon.cs
@@ -0,0 +1,7 @@
+namespace Library.Civilizaciones;
+
+public interface ICivilizaciones
+{
+    public double Bonificacion1 { get; set; }
+    public double Bonificacion2 { get; set; }
+}
---
+++ src/Library/Civilizaciones/Japoneses.cs
@@ -0,0 +1,26 @@
+namespace Library.Civilizaciones;
+
+public class Japoneses
+{
+    //bonificacion1: los aldeanos recolectan 20% mas rapido la comida
+    private double bonificacion1 = 1.20;
+
+    //bonificacion2: la infanteria ataca un 30% mas rapido
+    private double bonificacion2 = 1.30;
+
+    public double Bonificacion1
+    {
+        get
+        {
+            return this.bonificacion1;
+        }
+    }
+
+    public double Bonificacion2
+    {
+        get
+        {
+            return this.bonificacion2;
+        }
+    }
+}
