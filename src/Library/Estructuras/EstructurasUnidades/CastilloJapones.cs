3390f67b4065f46a90d2ef336c181716cfed2bb2
Facundo1105 <FACUNDO.ROSALES@CORREO.UCU.EDU.UY>
2025-06-30 04:40:19 -0300

    Arregle el crear unidades pasandolo al jugador para que cumpla con los principios

--- src/Library/Estructuras/EstructurasUnidades/CastilloJapones.cs
+++ src/Library/Estructuras/EstructurasUnidades/CastilloJapones.cs
@@ -1,88 +1,18 @@
 namespace Library;
 
 public class CastilloJapones : IEstructuras
-{ 
+{
     private int vida = 3000;
     public Celda CeldaActual { get; set; }
 
     public string Nombre
     {
-        get
-        {
-            return "Castillo Japones";
-        }
+        get { return "Castillo Japones"; }
     }
 
     public int Vida
     {
-        get
-        {
-            return this.vida;
-        }
-        set
-        {
-            this.vida = value < 0 ? 0 : value;
-        }
-    } 
-    public static void CrearUnidad(Jugador jugador)
-    {
-        bool noHayUnidadEspecial = true;
-
-        foreach (IUnidades unidadEspecial in jugador.EjercitoGeneral)
-        {
-            if (unidadEspecial is Elefante || unidadEspecial is JulioCesar || unidadEspecial is Samurai || unidadEspecial is Thor)
-            {
-                noHayUnidadEspecial = false;
-                break; // Salir del bucle si se encuentra una unidad especial
-            }
-        }
-
-        if (jugador.LimitePoblacion < 50 && jugador.CantidadUnidades < 30 && noHayUnidadEspecial)
-        {
-            // Obtener requisitos de recursos para el Samurai
-            ManejoDeRecursos manejoDe = new ManejoDeRecursos(400, 0, 0, 350);
-
-            // Sumar recursos disponibles
-            int oroTotal = 0;
-            int alimentoTotal = 0;
-
-            List<IEstructurasDepositos> depositosOro = new List<IEstructurasDepositos>();
-            List<IEstructurasDepositos> molinos = new List<IEstructurasDepositos>();
-            CentroCivico centroCivico = (CentroCivico)jugador.Estructuras[0];
-
-            foreach (IEstructuras estructura in jugador.Estructuras)
-            {
-                if (estructura is DepositoOro dOro)
-                {
-                    depositosOro.Add(dOro);
-                    oroTotal += dOro.EspacioOcupado;
-                }
-                else if (estructura is Molino molino)
-                {
-                    molinos.Add(molino);
-                    alimentoTotal += molino.EspacioOcupado;
-                }
-                else if (estructura is CentroCivico)
-                {
-                    oroTotal += centroCivico.RecursosDeposito["Oro"];
-                    alimentoTotal += centroCivico.RecursosDeposito["Alimento"];
-                }
-            }
-
-            // Verificar si tiene recursos suficientes
-            if (oroTotal >= manejoDe.CostoOro && alimentoTotal >= manejoDe.CostoAlimento)
-            {
-                int oroRestante = manejoDe.CostoOro;
-                int alimentoRestante = manejoDe.CostoAlimento;
-
-                // Descontar recursos de depósitos y centro cívico
-                ManejoDeRecursos.DescontarRecursos(depositosOro, centroCivico, oroRestante, "Oro");
-                ManejoDeRecursos.DescontarRecursos(molinos, centroCivico, alimentoRestante, "Alimento");
-
-                // Agregar la unidad al ejército del jugador
-                jugador.EjercitoGeneral.Add(new Samurai());
-            }
-        }
+        get { return this.vida; }
+        set { this.vida = value < 0 ? 0 : value; }
     }
-
 }
--- src/Library/Estructuras/EstructurasUnidades/Establo.cs
+++ src/Library/Estructuras/EstructurasUnidades/Establo.cs
@@ -9,89 +9,12 @@ public class Establo : IEstructuras
 
     public string Nombre
     {
-        get
-        {
-            return "Establo";
-        }
+        get { return "Establo"; }
     }
 
     public int Vida
     {
-        get
-        {
-            return this.vida;
-        }
-        set
-        {
-            this.vida = value < 0 ? 0 : value;
-        }
+        get { return this.vida; }
+        set { this.vida = value < 0 ? 0 : value; }
     }
-
-    public static void CrearUnidad(Jugador jugador)
-    {
-        if (jugador.LimitePoblacion < 50 && jugador.CantidadUnidades < 30)
-        {
-            // Obtener requisitos de recursos para Caballería
-            int costoOro = 200;
-            int costoAlimento = 300;
-            int costoMadera = 100;
-
-            if (jugador.Civilizacion is Romanos)
-            {
-                costoOro = (int)Math.Round(costoOro * 0.80);
-            }
-
-            // Sumar recursos disponibles
-            int oroTotal = 0;
-            int alimentoTotal = 0;
-            int maderaTotal = 0;
-
-            List<IEstructurasDepositos> depositosOro = new List<IEstructurasDepositos>();
-            List<IEstructurasDepositos> molinos = new List<IEstructurasDepositos>();
-            List<IEstructurasDepositos> depositosMadera = new List<IEstructurasDepositos>();
-            CentroCivico centroCivico = (CentroCivico)jugador.Estructuras[0];
-
-            foreach (IEstructuras estructura in jugador.Estructuras)
-            {
-                if (estructura is DepositoOro dOro)
-                {
-                    depositosOro.Add(dOro);
-                    oroTotal += dOro.EspacioOcupado;
-                }
-                else if (estructura is Molino molino)
-                {
-                    molinos.Add(molino);
-                    alimentoTotal += molino.EspacioOcupado;
-                }
-                else if (estructura is DepositoMadera dMadera)
-                {
-                    depositosMadera.Add(dMadera);
-                    maderaTotal += dMadera.EspacioOcupado;
-                }
-                else if (estructura is CentroCivico)
-                {
-                    oroTotal += centroCivico.RecursosDeposito["Oro"];
-                    alimentoTotal += centroCivico.RecursosDeposito["Alimento"];
-                    maderaTotal += centroCivico.RecursosDeposito["Madera"];
-                }
-            }
-
-            // Verificar si tiene recursos suficientes
-            if (oroTotal >= costoOro && alimentoTotal >= costoAlimento && maderaTotal >= costoMadera)
-            {
-                int oroRestante = costoOro;
-                int alimentoRestante = costoAlimento;
-                int maderaRestante = costoMadera;
-
-                // Descontar recursos de depósitos y centro cívico
-                ManejoDeRecursos.DescontarRecursos(depositosOro, centroCivico, oroRestante, "Oro");
-                ManejoDeRecursos.DescontarRecursos(molinos, centroCivico, alimentoRestante, "Alimento");
-                ManejoDeRecursos.DescontarRecursos(depositosMadera, centroCivico, maderaRestante, "Madera");
-
-                // Agregar la unidad al ejército del jugador
-                jugador.EjercitoGeneral.Add(new Caballeria());
-            }
-        }
-    }
-
 }
