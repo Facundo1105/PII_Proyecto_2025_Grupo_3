642e196c85b7cda9f00e6622dcb406c92fb390d3
Facundo1105 <FACUNDO.ROSALES@CORREO.UCU.EDU.UY>
2025-07-02 19:13:49 -0300

    Arreglo en metodos de atacar

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -13,10 +13,10 @@ class Program
     {
         // ICivilizaciones civilizacion1 = new Indios();
         // ICivilizaciones civilizacion2 = new Japoneses();
-        //Jugador jugador1 = new Jugador("Tomas");
-        //Jugador jugador2 = new Jugador("Gonzalo");
-        //Partida partida = new Partida(jugador1, jugador2);
-        //partida.IniciarPartida();
+        // Jugador jugador1 = new Jugador("Tomas");
+        // Jugador jugador2 = new Jugador("Gonzalo");
+        // Partida partida = new Partida(jugador1, jugador2);
+        // partida.IniciarPartida();
         // Mapa mapa = new Mapa();
         //
         // Fachada fachada = new Fachada(jugador1, jugador2, mapa);
